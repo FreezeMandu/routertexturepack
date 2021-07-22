1d8d690c18341451f0c92c78c63227d447ad4759
freezemandu <sewolminsuk03@gmail.com>
2021-07-23 00:55:29 +0900

    4

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-Vars.content.bullets().each(b=>b instanceof BasicBulletType, b=>b.frontRegion=Core.atlas.find("router"));
+Vars.content.bullets().each(b => 
+    b instanceof BasicBulletType, b => b.frontRegion = Core.atlas.find("router"));
+
+
+Vars.content.blocks.each(b => {
+    if(b.update) b.hasShadow = false;
+    });
