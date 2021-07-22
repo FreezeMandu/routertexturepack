7f20980ab83d0a8ddeb2e3a8bd41900150feb087
freezemandu <sewolminsuk03@gmail.com>
2021-07-23 01:09:41 +0900

    7

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 Events.on(EventType.ClientLoadEvent, e=>{
     Vars.content.bullets().each(b => b instanceof BasicBulletType, b => b.frontRegion = Core.atlas.find("router"));
+    });
+Events.on(EventType.ClientLoadEvent, e=>{
     Vars.content.bullets().each(b => b instanceof ArtilleryBulletType, b => b.frontRegion = Core.atlas.find("router"));
+    });
+Events.on(EventType.ClientLoadEvent, e=>{
     Vars.content.bullets().each(b => b instanceof BulletType, b => b.frontRegion = Core.atlas.find("router"));
     });
+    
+    
