582423dc8d98247d3833dadbc859d456a0aa6356
freezemandu <sewolminsuk03@gmail.com>
2021-07-23 00:48:31 +0900

    2

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
-Vars.content.bullets().each(b=>b instanceof BasicBulletType, b=>b.frontRegion=Core.atlas.find(Blocks.router));
+require('main.js');
+Vars.content.bullets().each(b=>b instanceof BasicBulletType, b=>b.frontRegion=Core.atlas.find("router"));
